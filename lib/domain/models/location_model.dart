dc9725ee86cab39a397e9f1253588ab9f069a0c6
Francesco Di Giacomo <francesco.digiacomo1@student.univaq.it>
2025-05-08 23:24:25 +0200

    modifica dei metodi fromFirestore e fromMap in location_model

--- lib/domain/models/location_model.dart
+++ lib/domain/models/location_model.dart
@@ -3,22 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Location{
   final String id;
   String name;
-  String nation;
-  String region;
-  String city;
-  String address;
-  double latitude;
-  double longitude;
 
   Location({
     required this.id,
     required this.name,
-    required this.address,
-    required this.nation,
-    required this.region,
-    required this.city,
-    required this.latitude,
-    required this.longitude,
   });
 
   factory Location.fromFirestore(DocumentSnapshot doc) {
@@ -26,39 +14,22 @@ class Location{
     return Location(
       id: doc.id,
       name: data['name'],
-      address: data['address'],
-      nation: data['nation'],
-      region: data['region'],
-      city: data['city'],
-      latitude: data['latitude'],
-      longitude: data['longitude'],
     );
   }
 
   /// Converte una mappa di Firestore in un oggetto Location
   factory Location.fromMap(Map<String, dynamic> map, String docId) {
     return Location(
-      id: docId,
-      name: map['name'],
-      address: map['address'],
-      nation: map['nation'],
-      region: map['region'],
-      city: map['city'],
-      latitude: map['latitude'],
-      longitude: map['longitude'],
+      id: map['locationId'],
+      name: map['locationName'],
     );
   }
 
   /// Converte un oggetto Location in una mappa per Firestore
   Map<String, dynamic> toMap() {
     return {
-      'name': name,
-      'address': address,
-      'nation': nation,
-      'region': region,
-      'city': city,
-      'latitude': latitude,
-      'longitude': longitude,
+      'locationName': name,
+      'locationId': id,
     };
   }
 
